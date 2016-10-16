bfcb9f77d212777eca6c0802cdf33fba63a115b2
Charger <epradeepep@gmail.com>
2016-10-16 18:53:13 -0500

    initial commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) { //allow cross origin requests
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(express.static(__dirname +"/tagg2016"));
+app.use(express.static(__dirname +"/public"));
 app.use(bodyparser.json());
 
 app.get('/getnews',function(req,res){
