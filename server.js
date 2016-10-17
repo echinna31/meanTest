eca5815d6e929dd5111e25c67d5041afd8add1ad
Charger <epradeepep@gmail.com>
2016-10-16 19:02:58 -0500

    initial commit-removing stuff

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
  * Created by Ch4rger on 10/15/2016.
  */
 var express = require('express');
-var mongojs = require('mongojs');
+//var mongojs = require('mongojs');
 var app = express();
-var db = mongojs('taggnews',['taggnews']);
+//var db = mongojs('taggnews',['taggnews']);
 var bodyparser =  require('body-parser');
 var port = Number(process.env.PORT || 3000);
 app.use(function(req, res, next) { //allow cross origin requests
@@ -15,47 +15,6 @@ app.use(function(req, res, next) { //allow cross origin requests
 });
 app.use(express.static(__dirname +"/public"));
 app.use(bodyparser.json());
-
-app.get('/getnews',function(req,res){
- var dbnews =   db.taggnews.find(function(err, docs){
-
-     console.log(docs);
-     res.json(docs)
- });
-
-})
-
-app.post('/postnews',function(req,res){
-    var dbnews =   db.taggnews.insert(req.body,function(err,doc){
-        res.json(doc)
-    });
-
-})
-
-
-app.delete('/deletenews/:id',function(req,res){
-    var id=req.params.id;
-  db.taggnews.remove({_id: mongojs.ObjectId(id)},function(err,d){
-        res.json(d);
-    });
-
-})
-
-app.get('/getnewsbyid/:id',function(req,res){
-    var id=req.params.id;
-    db.taggnews.findOne({_id: mongojs.ObjectId(id)},function(err,d){
-        res.json(d);
-    });
-
-})
-app.put('/updatenewsbyid/:id',function(req,res){
-    var id=req.params.id;
-    db.taggnews.findAndModify({query:{_id: mongojs.ObjectId(id)},update:{$set:{newsEntry:req.body.newsEntry}},new:true},function(err,d){
-        res.json(d);
-    });
-
-})
-
-
+console.log("server hit")
 
 app.listen(port );
